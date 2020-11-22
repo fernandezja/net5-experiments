72db4b0075c41778dfe3538e332c2f36de79435d
Jose A. Fernandez <fernandezja@gmail.com>
2020-11-22 10:54:28 -0300

    Example the read custom section from app settings into a console app

---
+++ 03-configuration/01-ReadCustomSectionConsoleApp/Config/StarwarsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ReadCustomSectionConsoleApp.Config
+{
+    public class StarwarsConfig
+    {
+        public string Side { get; set; }
+        public string Quote { get; set; }
+    }
+}
---
+++ 03-configuration/01-ReadCustomSectionConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using _01_ReadCustomSectionConsoleApp.Config;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace _01_ReadCustomSectionConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Read Custom Section from appsettings.json");
+			Console.WriteLine();
+
+			var builder = new ConfigurationBuilder();
+			
+			builder.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json", false);
+
+			IConfigurationRoot configuration = builder.Build();
+
+			var starwarsConfig = new StarwarsConfig();
+
+			IConfigurationSection section = configuration.GetSection("Starwars");
+
+			section.Bind(starwarsConfig);
+
+			Console.WriteLine($"Section Starwars");
+			Console.WriteLine($"	|_ Side = {starwarsConfig.Side}");
+			Console.WriteLine($"	|_ Quote = {starwarsConfig.Quote}");
+
+			Console.WriteLine();
+			Console.WriteLine($"Press any key to continue...");
+			Console.ReadKey();
+
+
+			
+        }
+    }
+}
